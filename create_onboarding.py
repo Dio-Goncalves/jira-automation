a4430bf8f5dfde617cb354c228d6f2178e355e70
Dio-Goncalves <diogoalexandresagoncalves@gmail.com>
2026-07-25 11:44:40 +0200

    Add Jira API authentication

--- create_onboarding.py
+++ create_onboarding.py
@@ -1,27 +1,32 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-from config import(
-	JIRA_URL,
-	JIRA_EMAIL,
-	JIRA_API_TOKEN,
+from config import (
+    JIRA_URL,
+    JIRA_EMAIL,
+    JIRA_API_TOKEN,
 )
 
 url = f"{JIRA_URL}/rest/api/3/myself"
 
 response = requests.get(
-	url,
-	auth=HTTPBasicAuth(JIRA_EMAIL, JIRA_API_TOKEN),
-	headers={
-		"Accept": "application/json"
-	}
+    url,
+    auth=HTTPBasicAuth(JIRA_EMAIL, JIRA_API_TOKEN),
+    headers={
+        "Accept": "application/json"
+    }
 )
 
 print(f"Status Code: {response.status_code}")
 
 if response.status_code == 200:
-	data = response.json()
-	print(f"Connected as: {data['displayName']}")
-	print(f"Account ID: {data['accountId']}")
+    me = response.json()
+
+    print()
+    print("Authentication Successful!")
+    print("--------------------------")
+    print(f"Name: {me['displayName']}")
+    print(f"Account ID: {me['accountId']}")
+    print(f"Email: {me.get('emailAddress', 'Hidden by Atlassian')}")
 else:
-	print(response.text)
+    print(response.text)
