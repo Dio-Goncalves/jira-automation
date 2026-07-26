ae0a6278fd23ff40bb5ee4b4e8ed6bd315a764b2
Dio-Goncalves <diogoalexandresagoncalves@gmail.com>
2026-07-26 19:04:10 +0200

    Update main.py
    Added comments

--- jira_training/main.py
+++ jira_training/main.py
@@ -1,8 +1,10 @@
 from jira_training.csv_reader import read_employees
 from jira_training.onboarding_service import create_employee_onboarding
 
+# This run the function that reads the CSV file and store all the values it finds in the 'employees' variable
 employees = read_employees()
 
+# Runs a loop where the imported 'create_employee_onboarding' script is executed for every employee value stored in the previously created 'employees' variable. This function then prints the responses for each onboarding request created
 for employee in employees:
     response = create_employee_onboarding(employee)
     print(response.status_code)
