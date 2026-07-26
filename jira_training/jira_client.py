158cf2dfd83c86db185313580549522cd590b3d3
Dio-Goncalves <diogoalexandresagoncalves@gmail.com>
2026-07-26 18:40:49 +0200

    Update jira_client.py
    Added comments to jira_client

--- jira_training/jira_client.py
+++ jira_training/jira_client.py
@@ -1,3 +1,5 @@
+# Centralized client for HTTP communication. Stores authentication and endpoint information.
+
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -7,14 +9,16 @@ from jira_training.config import (
     JIRA_API_TOKEN
 )
 
+# Authentication with imported data
 auth = HTTPBasicAuth(JIRA_EMAIL, JIRA_API_TOKEN)
 
+# Required headers for GET and POST requests
 headers = {
     "Accept": "application/json",
     "Content-Type": "application/json"
 }
 
-
+# Function that builds the payload to create request
 def create_requests(service_desk_id, request_type_id, fields):
     url = f"{JIRA_URL}/rest/servicedeskapi/request"
 
@@ -25,7 +29,7 @@ def create_requests(service_desk_id, request_type_id, fields):
     }
 
     from pprint import pprint
-    print("\nPayload being sent:")
+    print("Payload being sent:")
     pprint(payload)
     print
 
@@ -38,7 +42,7 @@ def create_requests(service_desk_id, request_type_id, fields):
 
     return response
 
-
+# Private helper function that performs GET requests to Jira
 def _get(endpoint):
     url = f"{JIRA_URL}{endpoint}"
 
@@ -49,20 +53,23 @@ def _get(endpoint):
     )
 
 
+# Function to retrieve every service desk available in Jira Service Management
 def get_service_desks():
     return _get("/rest/servicedeskapi/servicedesk")
 
 
+# This function gets request types for different service desk IDs without having to hardcode the endpoint URL
 def get_request_types(service_desk_id):
     return _get(
         f"/rest/servicedeskapi/servicedesk/{service_desk_id}/requesttype"
     )
 
 
+# Search Jira users
 def search_users(query):
     return _get(f"/rest/api/3/user/search?query={query}")
 
-
+# Search users and transform data into a python list
 def get_user_account_id(name):
     response = search_users(name)
 
